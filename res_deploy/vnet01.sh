5f8dda9f16b16a6050bdf00aa64213f4c07b7e49
Ruben Zhang <mcsezhang@gmail.com>
2024-02-22 13:48:05 -0500

    update file path

--- res_deploy/vnet01.sh
+++ res_deploy/vnet01.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 az deployment group create \
   --resource-group 'rg-tnsi-eastus-dev-01' \
-  --template-file vnet.bicep \
+  --template-file ../res_modules/vnet.bicep \
   --parameters virtualNetworkName='vnet-tnsi-eastus-01' subnet1Name='snet-tnsi-eastus-01' subnet2Name=snet-tnsi-eastus-02 \
   --parameters vnetAddressPrefix='10.100.0.0/16' subnet1AddressPrefix='10.100.0.0/24' subnet2AddressPrefix='10.100.1.0/24'
